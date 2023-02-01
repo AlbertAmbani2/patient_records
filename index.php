43892d9e6fa586be5848c0791fb0c02809c33983
albertambani2 <albertambani1@gmail.com>
2023-02-02 02:10:48 +0300

    init2

---
+++ index.php
@@ -0,0 +1,180 @@
+
+<?php
+include_once('patient_records/include/config.php');
+if(isset($_POST['submit']))
+{
+$name=$_POST['fullname'];
+$email=$_POST['emailid'];
+$mobileno=$_POST['mobileno'];
+$dscrption=$_POST['description'];
+$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+echo "<script>alert('Your information succesfully submitted');</script>";
+echo "<script>window.location.href ='index.php'</script>";
+
+} ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Patient Records System </title>
+
+    <link rel="shortcut icon" href="assets/images/fav.jpg">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawsom-all.min.css">
+     <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+    <body>
+
+    <!-- ################# Header Starts Here#######################--->
+    
+      <header id="menu-jk">
+    
+        <div id="nav-head" class="header-nav">
+            <div class="container">
+                <div class="row">
+                    <div style="color:#000;font-weight:bold; font-size:24px; margin-top: 1% !important;">Patient Records System
+                       <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
+                    </div>
+                    <div id="menu" class="navbar-nav ml-auto d-none d-md-block nav-item">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                               <li><a href="#logins">Logins</a></li>  
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    
+     <!-- ################# Slider Starts Here#######################--->
+
+    <div class="slider-detail">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+
+
+   
+
+
+            <div class="carousel-inner">
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="assets/images/homepage/home1.jpg" alt="Second slide">
+                    <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">Kitui Referral Hospital</h5>
+            
+                         
+                    
+                    </div>
+                </div>
+                
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets/images/homepage/home2.jpg" alt="Third slide">
+                      <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">Kitui Refferal Hospital</h5>
+            
+                         
+                    
+                    </div>
+              
+                </div>
+                
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+
+    </div>
+    
+  <!--  ************************* Logins ************************** -->
+    
+    
+     <section id="logins" class="our-blog container-fluid">
+        <div class="container">
+        <div class="inner-title">
+
+                <h2>Logins</h2>
+            </div>
+            <div class="col-sm-12 blog-cont">
+                <div class="row no-margin">
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                                <img src="assets/images/doctor.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>Doctors login</h6>
+                                <a href="patient_records/doctor" target="_blank">
+                                    <button class="btn btn-success btn-sm">Click Here</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                                <img src="assets/images/admin-login.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>Admin Login</h6>
+                    
+                                <a href="patient_records/admin" target="_blank">
+                                    <button class="btn btn-success btn-sm">Click Here</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    
+
+                    
+                    
+                </div>
+            </div>
+            
+        </div>
+    </section>  
+
+    
+    <div class="copy">
+            <div class="container">
+            Patient Records System
+                
+     
+            </div>
+
+        </div>
+    
+    </body>
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/jquery.easing.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/scrolling-nav.js"></script>
+<script src="assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
+
+<script src="assets/js/script.js"></script>
+
+
+
+</html>
---
+++ patient_records/doctor/add-payment.php
@@ -0,0 +1,221 @@
+<?php
+session_start();
+error_reporting(0);
+include('include/config.php');
+if(strlen($_SESSION['id']==0)) {
+ header('location:logout.php');
+  } else{
+
+if(isset($_POST['submit']))
+{	
+$patname=$_POST['patname'];
+$patcontact=$_POST['patcontact'];
+$code=$_POST['code'];
+$amount=$_POST['amount'];
+$sql=mysqli_query($con,"insert into tblpayment(PatientName,PatientContno,Code,Amount) values('$patname','$patcontact','$code','$amount')");
+if($sql)
+{
+echo "<script>alert('Payment info added Successfully');</script>";
+header('location:add-payment.php');
+
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Doctor | Add Payment</title>
+		
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+
+	<script>
+function userAvailability() {
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'email='+$("#code").val(),
+type: "POST",
+success:function(data){
+$("#user-availability-status1").html(data);
+$("#loaderIcon").hide();
+},
+error:function (){}
+});
+}
+</script>
+	</head>
+	<body>
+		<div id="app">		
+<?php include('include/sidebar.php');?>
+<div class="app-content">
+<?php include('include/header.php');?>
+						
+<div class="main-content" >
+<div class="wrap-content container" id="container">
+						<!-- start: PAGE TITLE -->
+<section id="page-title">
+<div class="row">
+<div class="col-sm-8">
+<h1 class="mainTitle">Patient | Add Payment</h1>
+</div>
+<ol class="breadcrumb">
+<li>
+<span>Payment</span>
+</li>
+<li class="active">
+<span>Add Payment</span>
+</li>
+</ol>
+</div>
+</section>
+							<div class="row">
+								<div class="col-md-12">
+									
+									<div class="row margin-top-30">
+										<div class="col-lg-6 col-md-12">
+											<div class="panel panel-white">
+												<div class="panel-heading">
+													<h5 class="panel-title">Make Payment</h5>
+												</div>
+												<div class="panel-body">	
+													<form role="form" name="" method="post" >
+														<div class="form-group">
+															<label for="exampleInputEmail1">
+																Enter Patient No.
+															</label>
+							<input type="number" name="doctorspecilization" class="form-control"  placeholder="Enter Patient No.">
+														</div>
+												
+														
+														
+														
+														<button type="submit"  class="btn btn-success">
+															Pay Now
+														</button>
+													</form>
+												</div>
+											</div>
+										</div>
+											
+											</div>
+										</div>
+									<div class="col-lg-12 col-md-12">
+											<div class="panel panel-white">
+												
+												
+											</div>
+										</div>
+									</div>
+<div class="container-fluid container-fullw bg-white">
+<div class="row">
+<div class="col-md-12">
+<div class="row margin-top-30">
+<div class="col-lg-8 col-md-12">
+<div class="panel panel-white">
+<div class="panel-heading">
+<h5 class="panel-title">Add Payment</h5>
+</div>
+<div class="panel-body">
+<form role="form" name="" method="post">
+
+<div class="form-group">
+<label for="doctorname">
+Patient Name
+</label>
+<input type="text" name="patname" class="form-control"  placeholder="Enter Patient Name" required="true">
+</div>
+<div class="form-group">
+<label for="fess">
+ Patient Contact no
+</label>
+<input type="text" name="patcontact" class="form-control"  placeholder="Enter Patient Contact no" required="true" maxlength="10" pattern="[0-9]+">
+</div>
+<div class="form-group">
+<label for="fess">
+Payment Code
+</label>
+<input type="text" id="code" name="code" class="form-control"  placeholder="Patient Payment Code" required="true" onBlur="userAvailability()">
+<span id="user-availability-status1" style="font-size:12px;"></span>
+</div>
+<div class="form-group">
+<label for="fess">
+ Amount
+</label>
+<input type="text" name="amount" class="form-control"  placeholder="Enter Patient Age" required="true">
+</div>
+
+<button type="submit" name="submit" id="submit" class="btn btn-success">
+Add
+</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-12 col-md-12">
+<div class="panel panel-white">
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>				
+</div>
+</div>
+</div>
+			<!-- start: FOOTER -->
+<?php include('include/footer.php');?>
+			<!-- end: FOOTER -->
+		
+			<!-- start: SETTINGS -->
+<?php include('include/setting.php');?>
+			
+			<!-- end: SETTINGS -->
+		</div>
+		<!-- start: MAIN JAVASCRIPTS -->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/modernizr/modernizr.js"></script>
+		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="vendor/switchery/switchery.min.js"></script>
+		<!-- end: MAIN JAVASCRIPTS -->
+		<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
+		<script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+		<script src="vendor/autosize/autosize.min.js"></script>
+		<script src="vendor/selectFx/classie.js"></script>
+		<script src="vendor/selectFx/selectFx.js"></script>
+		<script src="vendor/select2/select2.min.js"></script>
+		<script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+		<script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<!-- start: CLIP-TWO JAVASCRIPTS -->
+		<script src="assets/js/main.js"></script>
+		<!-- start: JavaScript Event Handlers for this page -->
+		<script src="assets/js/form-elements.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				FormElements.init();
+			});
+		</script>
+		<!-- end: JavaScript Event Handlers for this page -->
+		<!-- end: CLIP-TWO JAVASCRIPTS -->
+	</body>
+</html>
+<?php } ?>
